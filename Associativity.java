073dd7d1d6296df9a17e21b2e3614f9774b7dbf9
Tarun Bavra <tarunbavra@gmail.com>
2025-01-28 15:35:28 +0530

    first-commit

---
+++ Associativity.java
@@ -0,0 +1,16 @@
+public class Associativity {
+    public static void main(String[] args) {
+        int[][] A = { { 5, 6 }, { 2, 3 }, { 8, 9 } };
+        int[][] B = { { 7, 8 }, { 4, 5 }, { 3, 2 } };
+        int[][] C = { { 1, 4 }, { 2, 9 }, { 3, 8 } };
+
+        // computing (A*B)*C
+        int[][] AB = multiplyMatrices(A, B);
+
+    }
+
+    public static multiplyMatrices(int[][] A, int[][] B){
+
+    }
+
+}
